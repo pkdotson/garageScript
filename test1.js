f1d163b361a7b1cdc7adf247887fcfaa7c6f1864
pkdiddy30 <pkdotson@gmail.com>
2017-08-14 19:36:44 -0700

    remaining changes

--- test1.js
+++ test1.js
@@ -1,7 +1,7 @@
 const add = require('./algo1.js');
 
-const test = (num1,num2,total) => {
-  if(add(num1,num2) === total){
+const test = (num1, num2, total) => {
+  if(add(num1, num2) === total){
     console.log('correct');
   } else {
     console.log('incorrect');
