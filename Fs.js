94395de678454cbc69f9a147f6fc9989ecd78580
Phillip Kelley-Dotson <pdiddy@llip.life>
2017-08-15 17:18:25 -0700

    fix algorithm

--- Fs.js
+++ Fs.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 
+let s = '';
+
 const names = (name) => {
   if(name != 'Phillip'){
-    console.log(name);
+  s = s + name; 
   }
 }
 
 const allNames = (err, files) => {
   files.forEach(names);
+  fs.writeFile('prntname.txt', s, (err) => {
+    console.log(err);
+  });
 }
-
 fs.readdir('/home', allNames);
