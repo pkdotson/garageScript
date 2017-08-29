d6cdc57505d726828556cbd4bc649adf24755267
Phillip Kelley-Dotson <pdiddy@llip.life>
2017-08-28 19:10:26 -0700

    fix mr

--- publicHtml/ajax.js
+++ publicHtml/ajax.js
@@ -5,7 +5,7 @@ const app = express();
 app.listen(3619);
 app.use(express.static('public'));
 
-app.get('/XMLHttpRequest', (req, res) => {
+app.get('/newrequest', (req, res) => {
   fs.appendFile('/home/pdiddy/garageScript/publicHtml/public/xmlpublicreq.txt',`\n Name: ${req.query.name} Comment: ${req.query.comment} \n`);
   res.send('Your comment has be recieved');
 });
